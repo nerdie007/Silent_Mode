a247d699659b3d5fa8cefb28ec2a6e40e8c3373f
nerdie007 <brucker_k2@yahoo.de>
2018-03-28 10:30:54 +0200

    Lektion 1

---
+++ app/src/main/java/com/example/bojan/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.bojan.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+/**
+ * Created by Bojan on 28.03.2018.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
